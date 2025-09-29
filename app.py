4c70c9801d98429c3188e228062f87bd7e8bf97d
hannes <hannes.mitterer@gmail.com>
2025-09-29 02:07:48 -0400

    feat: unpack and commit all files from euystacio_dashboard_package.tar.gz for Render deployment

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
-import os
+# app.py
+
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'seedbringer-default-key')
-socketio = SocketIO(app)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-@socketio.on('verify_tranche')
-def handle_verify_tranche(data):
-    emit('tranche_verified', data, broadcast=True)
-
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=5000)
